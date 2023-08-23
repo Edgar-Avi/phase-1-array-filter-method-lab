1fafcf47df156e2057ff6297556ae5eb433abc58
Edgar Avila <edgaravila01@yahoo.com>
2023-08-23 10:13:48 -0700

    phase-1 array filter method lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+// Code your solution herenpm test
+
+function findMatching(drivers, name) {
+    return drivers.filter((drivers) => {
+        if(drivers.toLowerCase() === name.toLowerCase()) {
+            return true;
+        }
+    })
+}
+
+
+function fuzzyMatch(collection, string) {
+    return collection.filter((driver) => {
+        if (driver.toLowerCase().startsWith(string.toLowerCase())) {
+            return true;
+        }
+    })
+}
+
+function matchName(DriverObj, name) {
+    return DriverObj.filter((DriverObj) => {
+        if (DriverObj.name.toLowerCase() === name.toLowerCase()) {
+            return true;
+        }
+    })
+  
+}
